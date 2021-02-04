1efdd8e1cb0ea2406d3434d0504c90900d37a4a4
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-04 12:45:37 +0530

    [Dipesh] modify ClassPersonAdded

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -2,6 +2,17 @@
 
 namespace AddressBookSystem
 {
+    class Person
+    {
+        string firstName;
+        string lastName;
+        string address;
+        string city;
+        string state;
+        string zip;
+        string phoneNumber;
+        string email;
+    }
     class Program
     {
         static void Main(string[] args)
