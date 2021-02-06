ad11148f36a6541e254cab0dc967d8ba620bc500
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-06 09:23:04 +0530

    [Dipesh] refactor RefactoredClasses

---
+++ AddressBookSystem/AddressBookCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    public class AddressBookCollection
+    {
+        public Dictionary<string, AddressBook> addressBookDictionary;
+        public AddressBookCollection()
+        {
+            addressBookDictionary = new Dictionary<string, AddressBook>();
+        }
+        public void PrintAllAddressBookNames()
+        {
+            foreach (var AddressBookItem in addressBookDictionary)
+            {
+                Console.WriteLine(AddressBookItem.Key);
+            }
+        }
+    }
+
+}
---
+++ AddressBookSystem/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string zip;
+        public string phoneNumber;
+        public string email;
+
+        public void DisplayPerson()
+        {
+            Console.WriteLine($"First Name : {firstName}");
+            Console.WriteLine($"Last Name : {lastName}");
+            Console.WriteLine($"Address : {address}");
+            Console.WriteLine($"City : {city}");
+            Console.WriteLine($"State : {state}");
+            Console.WriteLine($"Zip : {zip}");
+            Console.WriteLine($"PhoneNumber : {phoneNumber}");
+            Console.WriteLine($"Email : {email}");
+        }
+    }
+}
