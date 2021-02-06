3c8061de59e605ce0b456c9f56926e995663d64e
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-06 09:14:21 +0530

    [Dipesh] modify AccomodatedForMultipleAddressBooks

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -123,12 +123,33 @@ namespace AddressBookSystem
                 Console.WriteLine("Entry Not found");
         }
     }
+
+    public class AddressBookCollection
+    {
+        public Dictionary<string, AddressBook> addressBookDictionary;
+        public AddressBookCollection()
+        {
+            addressBookDictionary = new Dictionary<string, AddressBook>();
+        }
+        public void PrintAllAddressBookNames()
+        {
+            foreach (var AddressBookItem in addressBookDictionary)
+            {
+                Console.WriteLine(AddressBookItem.Key);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            AddressBook addressBook = new AddressBook();
+            
             Console.WriteLine("Welcome to Address Book!");
+            Console.WriteLine("Enter Default Address Book Name");
+            string addressBookName = Console.ReadLine();
+            AddressBookCollection addressBookCollection = new AddressBookCollection();
+            AddressBook addressBook = new AddressBook();
+            addressBookCollection.addressBookDictionary.Add(addressBookName, addressBook);
             int choice;
             do
             {
@@ -139,37 +160,53 @@ namespace AddressBookSystem
                 Console.WriteLine("2) Insert new Entry");
                 Console.WriteLine("3) Edit Entry");
                 Console.WriteLine("4) Delete Entry");
-                Console.WriteLine("5) Exit");
+                Console.WriteLine("5) Add New Address Book");
+                Console.WriteLine("6) Switch To Different Address Book");
+                Console.WriteLine("7) Exit");
+                Console.WriteLine("------------------------------");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        addressBook.DisplayNamesInAddresBook();
+                        addressBookCollection.addressBookDictionary[addressBookName].DisplayNamesInAddresBook();
                         break;
                     case 2:
-                        addressBook.AddAddressBookEntry();
+                        addressBookCollection.addressBookDictionary[addressBookName].AddAddressBookEntry();
                         break;
                     case 3:
                         Console.WriteLine("Enter First Name");
                         string firstName = Console.ReadLine();
                         Console.WriteLine("Enter Last Name");
                         string lastName = Console.ReadLine();
-                        addressBook.EditContact(firstName,lastName);
+                        addressBookCollection.addressBookDictionary[addressBookName].EditContact(firstName,lastName);
                         break;
                     case 4:
                         Console.WriteLine("Enter First Name");
                         firstName = Console.ReadLine();
                         Console.WriteLine("Enter Last Name");
                         lastName = Console.ReadLine();
-                        addressBook.DeleteContact(firstName, lastName);
+                        addressBookCollection.addressBookDictionary[addressBookName].DeleteContact(firstName, lastName);
                         break;
                     case 5:
+                        Console.WriteLine("Enter New Address Book Name");
+                        addressBookName = Console.ReadLine();
+                        addressBookCollection.addressBookDictionary.Add(addressBookName, new AddressBook());
+                        Console.WriteLine($"Address Book {addressBookName} selected!");
+                        break;
+                    case 6:
+                        Console.WriteLine("Listing all Address Books");
+                        foreach (var addressBookEntry in addressBookCollection.addressBookDictionary)
+                        {
+                            Console.WriteLine(addressBookEntry.Key);
+                        }
+                        Console.WriteLine("Select an Address Book");
+                        addressBookName = Console.ReadLine();
                         break;
                     default:
                         Console.WriteLine("Enter Proper Choice!");
                         break;
                 }
-            } while (choice!=5);
+            } while (choice!=7);
             
            
         }
